2cc914a23615c86e8b97d9a8ef2cd29e1045c102
khushboo khatwani <khushboo@vicdigit-technologies.com>
2023-06-27 21:25:54 +0200

    completed layout for Home Trend and More Section -css

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -11,19 +11,19 @@ function Header() {
    
             <img className="h-full" src={Companylogo} alt="companylogo"/>
         
-        <div className="flex">
-            <div className='flex  items-center border-2 border-slate-900 ml-524'>
-                    <div>
-                        <img src={Home} alt="Home"/>
-                        <div>Home</div>
+        <div className="flex justify-center items-center ">
+            <div className='flex  items-center border-2 border-slate-900 ml-524 h-[1.6875rem] w-[21.3125rem]'>
+                    <div className='flex items-center justify-between w-[4.875rem]'>
+                        <img className='w-[1.125rem]' src={Home} alt="Home"/>
+                        <div className='w-[3.125rem] h=[1.6875rem]'>Home</div>
                     </div>
-                    <div className='ml-10'>
-                        <img src={Toptrend} alt="toptrendlogo"/>
-                        <div>Top Trends</div>
+                    <div className='ml-10 flex justify-center items-center w-[7.3125rem]  '>
+                        <img className="w-6 h-6" src={Toptrend} alt="toptrendlogo"/>
+                        <div className="w-[5.1875rem] ">Top Trends</div>
                     </div>
-                    <div className="flex items-center justify-between className='ml-10'">
-                        <div>More</div>
-                        <img src={Arrowdown} alt="arrowdownlogo"/>
+                    <div className="flex items-center justify-between ml-10 w-[3.5625rem]">
+                        <div clasName="w-[2.5625rem] h-3">More</div>
+                        <img clasName="h-3.5 w-[0.5625rem]"src={Arrowdown} alt="arrowdownlogo"/>
                     </div>
 
             </div>
@@ -39,7 +39,6 @@ function Header() {
             </div>
 
         </div>
-
     </main>
   )
 }
