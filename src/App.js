c2c35be58c2eb8f933f6dc0ce7ad31674f171ea3
khushboo khatwani <khushboo@vicdigit-technologies.com>
2023-06-29 23:43:43 +0200

    Trends Near You Comp Completed

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import BecomeSeller from "./Component/BecomeSeller";
 import WhyOnlyWe from "./Component/WhyOnlyWe"
 import MostTrending from "./Component/MostTrending";
 import TodaysDeal from "./Component/TodaysDeal";
+import TrendNearrYou from "./Component/TrendNearrYou";
 
 function App() {
   return (
     <main >
      <Header/>
+     <TrendNearrYou/>
      <TodaysDeal/>
      <MostTrending/>
      <WhyOnlyWe/>
--- src/Component/TodaysDeal.js
+++ src/Component/TodaysDeal.js
@@ -7,7 +7,7 @@ function TodaysDeal() {
             <h1 className="text-[#1f1f1f] text-[2.3125rem] font-semibold tracking-tight " >Todays Deal</h1>
             <div className='relative rounded-[1.25rem] h-[30.125rem] w-[89.5rem]'>
                 <img className="w-full h-full" src={TodaysDealImg}alt="TodaysDealImage"/>
-                <div className='absolute bottom-[1.56rem] right-[9.12rem] left-[56.62rem] top-[24.81rem] rounded-[0.625rem] bg-[#484FA2] text-white text-[2.3125rem] font-semibold tracking-tighter text-center'><a className='flex justify-center items-center' href="#">Explore</a></div>
+                <div className='absolute bottom-[1.56rem] right-[9.12rem] left-[56.62rem] top-[24.81rem] w-[23.75rem] h-[3.75rem] rounded-[0.625rem] bg-[#484FA2] text-white text-[2.3125rem] font-semibold tracking-tighter text-center'><a className='flex justify-center items-center' href="#">Explore</a></div>
             </div>
 
     </section>
