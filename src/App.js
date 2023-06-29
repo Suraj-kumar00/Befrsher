2e2838023c963da6262a7e47bc777acf7e7956b5
khushboo khatwani <khushboo@vicdigit-technologies.com>
2023-06-29 19:07:26 +0200

    Becomeseller comp completed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Footer from "./Component/Footer";
 import Header from "./Component/Header";
 import InstagramComp from "./Component/InstagramComp";
 import { Newsletter } from "./Component/Newsletter";
+import BecomeSeller from "./Component/BecomeSeller";
 
 function App() {
   return (
     <main >
      <Header/>
+     <BecomeSeller/>
      <InstagramComp/>
      <Newsletter/>
      <Footer/>
