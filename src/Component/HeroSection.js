7e6b155387fb661a27fe85445b4e5aa7f565604e
khushboo khatwani <khushboo@vicdigit-technologies.com>
2023-06-30 10:42:04 +0200

    herosec struct and stylig done

--- src/Component/HeroSection.js
+++ src/Component/HeroSection.js
@@ -2,30 +2,34 @@ import React from 'react'
 import searchicon from "../Images/searchicon.svg"
 import Locationicon from "../Images/Location icon.svg"
 import HeroSecImg from "../Images/HeroSecImg.png"
+import storeicon from "../Images/Store.png"
 
 function HeroSection() {
   return (
-    <section className="w-[89.5rem] h-[26.5rem] flex flex-col border-2 border-yellow-400">
-        <img  className="relative w-screen h-full" src={HeroSecImg} alt="HEroSecImg"/>
-        <div className='absolute'>
-             <p className="text-[#E7e7e7] absolute text-[3.125rem] font-bold -tracking-[0.3125rem] h-[12.5rem] w-[74.3125rem] top-[4.06rem] bottom-[9.94rem] right-[10.19rem] left-[10rem] border-2 border-green-400 text-center">Keep Up with Local Fashion<br></br>
- Trends. EXPL  RE</p>
-            <div className="searchlocation w-[44rem] h-10 rounded-[0.625rem] bg-[#e7e7e7] absolute top-[16.63rem] bottom-[7.38rem] right-[25.25rem] left-[25.25rem]">
-                <div className="searchlocation w-[44rem] h-10 rounded-[0.625rem] bg-[#e7e7e7] absolute top-[16.63rem] bottom-[7.38rem] right-[25.25rem] left-[25.25rem]">
-                            <div className="">
-                                <img className=" h-4" src={Locationicon} alt="locationicon"/>
-                                <div className=""type="text" placeholder='Add Location'>Add Location </div>
+    <section className="w-[89.5rem] h-[26.5rem] flex flex-col  items-center justify-center">
+        <img  className="relative w-full h-full" src={HeroSecImg} alt="HEroSecImg"/>
+        <div className='absolute flex flex-col items-center '>
+             <p className="text-[#E7e7e7]  text-[3.125rem] flex items-center justify-center font-bold -tracking-[0.3125rem] h-[12.5rem] w-[74.3125rem]  ">Keep Up with Local Fashion<br></br>
+                                    Trends. EXPL  RE</p>
+            <div className=" relative w-[44rem] h-10 rounded-[0.625rem] bg-[#e7e7e7] flex items-center ">
+                
+                            <div className="flex w-[8rem] h-5 justify-center ml-[0.5rem]">
+                                <img className=" h-4 flex j" src={Locationicon} alt="locationicon"/>
+                                <div className="flex items-center  ml-1 w-[7.25rem] h-5"type="text" placeholder='Add Location'>Add Location </div>
 
                             </div>
-                             <div className="s w-[0.0625rem] h-[2.5rem] bg-[#8a8a8a]"></div>
+                             <div className="w-[0.0625rem] h-10 bg-[#8a8a8a] flex ml-[5.64rem]" ></div>
                                  
-                                    <div className="searchfunc">
+                                    <div className=" flex items-center  ml-[1.5rem]">
                                         <img src={searchicon} alt="searchicon"/>
-                                            <label>
+                                            <label className=" ml-1">
                                                     <input type="submit" className="searchbtn" placeholder='Search here' value="Search Here"></input>            
-                                        </label> 
+                                         </label> 
 
                                     </div>
+                                    <img src={storeicon} alt="storeicon" className="w-[1.875rem] h-[1.875rem] absolute left-[41.13rem] right-[0.31rem]"/>
+
+
                                         
 
              </div>
