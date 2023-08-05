4c1d724193dbfafea26b253804eb708376ebb676
khushboo khatwani <khushboo@vicdigit-technologies.com>
2023-08-05 23:05:25 +0200

    responsive prop adjustment

--- src/Component/SetUpYourStyle.js
+++ src/Component/SetUpYourStyle.js
@@ -75,17 +75,17 @@ function SetUpYourStyle() {
  
    
   return (
-    <div className="grid justify-between  grid-cols-2 sm:grid-cols-1 4xl:h-vh 2xl:text-[1.3125rem] 4xl:text-[2.3125rem]">
+    <div className="grid justify-between  grid-cols-2 sm:grid-cols-1 4xl:h-vh 2xl:text-[1.3125rem] 4xl:text-[2.3125rem] border-8 border-green-700">
           <div className="shadow-my_shadow rounded-[2rem]  max-w-full  md:max-h-full  2xl:max-h-[62.625rem] 4xl:max-h-full sm:block 4xl:hidden" >
           <img className=" h-full rounded-[2rem]" src={formgirlimage} alt="girlimg"/>
         </div>
 
-      <div  className=" 2xl:max-h-[62.625rem] flex sm:mt-4 md:max-h-full  4xl:max-h-full 4xl:max-w-full">
+      <div  className=" 2xl:max-h-[62.625rem] flex sm:mt-4 md:max-h-full  4xl:max-h-full 4xl:max-w-full border-2 border-black">
       <Formik
     initialValues={initialValues}
     validationSchema = {validationSchema}
     onSubmit = {onSubmit}>  
-        <Form  className="p-8 xl:p-4 xl:mx-4 lg:mx-1 ml-[6.5rem]  2xl:max-w-[40.25rem] 4xl:max-w-full 4xl:w-full max-h-[62.625rem] 4xl:max-h-full 4xl:mr-[6.56rem] ">
+        <Form  className="p-8 xl:p-4 xl:mx-4 lg:mx-1 2xl:ml-[6.5rem] 4xl:ml-[5rem]  2xl:max-w-[40.25rem] 4xl:max-w-full  max-h-[62.625rem] 4xl:max-h-full 4xl:mr-[6.56rem] border-2 border-red-800 ">
         <div className='flex justify-between sm:flex-col  '>       
 
         <div onClick={SignupHandler}
@@ -93,7 +93,7 @@ function SetUpYourStyle() {
             {  "text-white bg-[#484FA2]" : activeButtonIndex===1,
              "text-black" : activeButtonIndex===0
             } ,
-            ' flex justify-center items-center sm:max-w-full  2xl:max-w-[12.5rem] h-[3.125rem] w-full 4xl:max-w-[16.5rem] 4xl:max-h-[6.125rem] rounded-[0.3125rem]  text-[#fff] 2xl:text-[1.3125rem] 4xl:text-[2.3125rem] border-[#484FA2] border-[1px] font-semibold leading-1.31 tracking-light ]'
+            ' flex justify-center items-center sm:max-w-full  2xl:max-w-[12.5rem] 2xl:max-h-[3.125rem] w-full 4xl:max-w-[16.5rem] 4xl:max-h-[6.125rem] rounded-[0.3125rem]  text-[#fff] 2xl:text-[1.3125rem] 4xl:text-[2.3125rem] border-[#484FA2] border-[1px] font-semibold leading-1.31 tracking-light ]'
             )}>Sign Up</div>
         <div onClick={LoginHandler}
         className={clsx(
@@ -121,9 +121,9 @@ function SetUpYourStyle() {
 
             <div className="flex flex-col justify-start mt-[2.25rem] sm1:mt-4">    
                  <p className='text-[#8a8a8a] 2xl:text-[1rem] font-medium leading-4 -tracking-tight 4xl:text-[2rem] 2xl:mb-1 4xl:mb-2'>Gender *</p>
-                  <div className="flex  2xl:justify-between 4xl:gap-14 4xl:items-center  mt-[0.75rem] md:flex-col">
+                  <div className="flex  2xl:justify-between 4xl:gap-14 md:gap-0 4xl:items-center  mt-[0.75rem] md:flex-col">
                               <button type="button"
-                                className=' flex justify-center items-center md:mt-1 4xl:text-[2.5rem] 4xl:py-[2rem] 4xl:px-[2rem]  lg:px-7 2xl:max-w-full 4xl:max-w-[10rem] max-h-[55.8125rem] border-[#8a8a8a] border-[0.06rem] 2xl:py-[1.06rem] px-[2.63rem] 2xl:text-[1rem] font-normal leading-4 -tracking-tight rounded-[0.3125rem]'
+                                className=' flex justify-center items-center  md:mt-1 4xl:text-[2.5rem] 4xl:py-[2rem] 4xl:px-[2rem]  lg:px-7 2xl:max-w-full md:w-full 4xl:max-w-[10rem] max-h-[55.8125rem] border-[#8a8a8a] border-[0.06rem] 2xl:py-[1.06rem] px-[2.63rem] 2xl:text-[1rem] font-normal leading-4 -tracking-tight rounded-[0.3125rem]'
                                 name="Male"
                                 // type="button"
                                 onClick={(event) => handleClick(event, "Male")}
@@ -132,14 +132,14 @@ function SetUpYourStyle() {
                               </button> 
 
                                <button type="button"
-                                className= 'flex justify-center items-center md:mt-1 4xl:text-[2.5rem] 4xl:py-[2rem] 4xl:px-[2rem]  lg:px-7 2xl:max-w-full 4xl:max-w-[10rem] max-h-[55.8125rem] border-[#8a8a8a] border-[0.06rem] 2xl:py-[1.06rem] px-[2.63rem] 2xl:text-[1rem] font-normal leading-4 -tracking-tight rounded-[0.3125rem]'
+                                className= 'md:w-full flex justify-center items-center md:mt-1 4xl:text-[2.5rem] 4xl:py-[2rem] 4xl:px-[2rem]  lg:px-7 2xl:max-w-full 4xl:max-w-[10rem] max-h-[55.8125rem] border-[#8a8a8a] border-[0.06rem] 2xl:py-[1.06rem] px-[2.63rem] 2xl:text-[1rem] font-normal leading-4 -tracking-tight rounded-[0.3125rem]'
 
                                 name="Female"
                               >
                                 Female
                               </button>
                                <button type="button"
-                                className=' flex justify-center items-center md:mt-1 4xl:text-[2.5rem] 4xl:py-[2rem] 4xl:px-[2rem]  lg:px-7 2xl:max-w-full 4xl:max-w-[10rem] max-h-[55.8125rem] border-[#8a8a8a] border-[0.06rem] 2xl:py-[1.06rem] px-[2.63rem] 2xl:text-[1rem] font-normal leading-4 -tracking-tight rounded-[0.3125rem]'
+                                className='md:w-full  flex justify-center items-center md:mt-1 4xl:text-[2.5rem] 4xl:py-[2rem] 4xl:px-[2rem]  lg:px-7 2xl:max-w-full 4xl:max-w-[10rem] max-h-[55.8125rem] border-[#8a8a8a] border-[0.06rem] 2xl:py-[1.06rem] px-[2.63rem] 2xl:text-[1rem] font-normal leading-4 -tracking-tight rounded-[0.3125rem]'
                                 name="Special"
                               >
                                 Special
@@ -151,11 +151,11 @@ function SetUpYourStyle() {
             
             <div className='flex 4xl:flex-row 4xl:justify-between  md:flex-col mt-[2.25rem] md:mt-5'>
                     <div className='flex flex-col   '>
-                    <Field className="flex text-[#8a8aa8] border-[0.06rem] border-[#8a8a8a] md:max-w-full lg:max-w-[10rem] 4xl:max-w-full 2xl:max-w-[13.125rem] 2xl:max-h-[3.125rem] 4xl:max-h-[6.125rem] py-[1.03rem]  pl-[1.56rem] rounded-[0.3125rem]"  type="text" id="city" name="city" placeholder="City *" />
+                    <Field className="flex text-[#8a8aa8] border-[0.06rem] border-[#8a8a8a] md:max-w-full lg:max-w-[10rem] 4xl:max-w-[16rem] 2xl:max-w-[13.125rem] 2xl:max-h-[3.125rem] 4xl:max-h-[6.125rem] py-[1.03rem]  pl-[1.56rem] rounded-[0.3125rem]"  type="text" id="city" name="city" placeholder="City *" />
                     <ErrorMessage name='city' component="div" className="text-red-500 2xl:text-xs italic 4xl:text-2xl"/>
                     </div>
                     <div className='flex flex-col '>
-                    <Field className=" md:mt-1 flex text-[#8a8aa8] border-[0.06rem] border-[#8a8a8a] md:max-w-full lg:max-w-[10rem] 4xl:max-w-full 2xl:max-w-[13.125rem]  2xl:max-h-[3.125rem] 4xl:max-h-[6.125rem] py-[1.03rem] pl-[1.56rem] rounded-[0.3125rem]"  type="text" id="area" name="area" placeholder="Area *" />
+                    <Field className=" md:mt-1 flex text-[#8a8aa8] border-[0.06rem] border-[#8a8a8a] md:max-w-full lg:max-w-[10rem] 4xl:max-w-[16rem] 2xl:max-w-[13.125rem]  2xl:max-h-[3.125rem] 4xl:max-h-[6.125rem] py-[1.03rem] pl-[1.56rem] rounded-[0.3125rem]"  type="text" id="area" name="area" placeholder="Area *" />
                               <ErrorMessage name='area' component="div" className="text-red-500 2xl:text-xs italic 4xl:text-2xl"/>
                     </div>
             </div>
