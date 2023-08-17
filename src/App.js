6c4a2b59e284d5ab86ded3a8e67fb0c9522f78c3
suraj kumar <suraj.ukumar.p@gmail.com>
2023-08-17 15:10:31 +0530

    app.js updated

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import Header from "./Component/Header";
 import InstagramComp from "./Component/InstagramComp";
 import { Newsletter } from "./Component/Newsletter";
 import BecomeSeller from "./Component/BecomeSeller";
-import WhyOnlyWe from "./Component/WhyOnlyWe"
+import WhyOnlyWe from "./Component/WhyOnlyWe";
 import MostTrending from "./Component/MostTrending";
 import TodaysDeal from "./Component/TodaysDeal";
 import TrendNearrYou from "./Component/TrendNearrYou";
 import Categories from "./Component/Categories";
 import HeroSection from "./Component/HeroSection";
 import SignupForm from "./Component/SignupForm";
-
-import LoginForm from "./Component/LoginForm"
+import LoginForm from "./Component/LoginForm";
 import DummyHeaderForSignupForm from "./Component/dummyHeaderForSignupForm";
 import HeaderForSignUpPage from "./Component/HeaderForSignUpPage";
 import SetUpYourStyle from "./Component/SetUpYourStyle";
@@ -25,41 +24,25 @@ import BodyHelpCenterHome from "./Component/BodyHelpCenterHome";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <main>
-      <HeaderHelpCenterHome />
-      <BodyHelpCenterHome />
-      {/* <HeaderAfterLogin /> */}
-      {/* <HeaderWithoutLogin />
-      <AboutUsWithLogin /> */}
-      {/* <BodyTermandCondtions /> */}
-=======
-    <main className="container-snap  h-screen box-border w-screen text-base" >
-{/*     
+    <main className="container-snap  h-screen box-border w-screen text-base">
+      {/*     
       <DummyHeaderForSignupForm/>
      <SignupForm/>   */}
-     {/* <SignupForm1/> */}
-     {/* <Header/>
+      {/* <SignupForm1/> */}
+      {/* <Header/>
      <Categories/>
      <HeroSection/>
      <HeaderForSignUpPage/> */}
-
-     {/* <LoginForm/> */}
-     {/* <SetUpYourStyle/> */}
-     
-    
+      {/* <LoginForm/> */}
+      {/* <SetUpYourStyle/> */}
       <HeaderAfterLogin />
       {/* <AboutUsWithLogin /> */}
       <BodyTermandCondtions />
->>>>>>> 2823e041d2d2f3fe11e365b02d5296bf140fc0e4
       {/* <Header />
-
-
       <DummyHeaderForSignupForm />
       <SignupForm1 />
       <HeaderForSignUpPage />
       <LoginForm />
-
       <SignupForm />
       <SetUpYourStyle />
       <HeroSection />
@@ -71,27 +54,11 @@ function App() {
       <BecomeSeller />
       <InstagramComp />
       <Newsletter />
-
-<<<<<<< HEAD
-      {/* <DummyHeaderForSignupForm />
-      <SignupForm />
-      <Header />
-=======
-      
->>>>>>> 8a5ca6a18c66a1bde29888c11cd4d2e6ea32f124
-
-<<<<<<< HEAD
-      <LoginForm /> */}
-      {/* <SetUpYourStyle /> */}
-      <Footer />
-=======
       {/* <LoginForm/> */}
       {/* <SetUpYourStyle/> */}
-      {/* <Footer /> */}
->>>>>>> 2823e041d2d2f3fe11e365b02d5296bf140fc0e4
-
+      <Footer />
     </main>
   );
 }
 
-export default App
+export default App;
